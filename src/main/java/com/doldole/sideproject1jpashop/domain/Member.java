8da9c69cf7998df77cbb5b78c569283a1fc27f2b
Ican <cs2772@naver.com>
2021-04-03 14:44:01 +0900

    회원 등록, 수정, 조회 API 추가

--- src/main/java/com/doldole/sideproject1jpashop/domain/Member.java
+++ src/main/java/com/doldole/sideproject1jpashop/domain/Member.java
@@ -1,9 +1,11 @@
 package com.doldole.sideproject1jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
